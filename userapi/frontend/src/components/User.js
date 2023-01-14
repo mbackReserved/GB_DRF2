723f6aeb767590ce25e12b07ce80805b91f52028
mbackReserved <lowmustgoon@gmail.com>
2023-01-14 09:30:14 +0300

    create components folder and add UserList

---
+++ userapi/frontend/src/components/User.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const UserItem = ({user}) => {
+    return (
+        <tr>
+            <td>
+                {user.firstname}
+            </td>
+            <td>
+                {user.lastname}
+            </td>
+            <td>
+                {user.email}
+            </td>
+        </tr>
+    )
+}
+
+const UserList = ({users}) => {
+    return (
+        <table>
+            <th>
+                First name
+            </th>
+            <th>
+                Last name
+            </th>
+            <th>
+                Email
+            </th>
+            {users.map((user) => <UserItem user={user} />)}
+        </table>
+    )
+}
+
+export default UserList
