8fd3e3970b199d89d50c1b1e88c8d47cd8c60de6
mbackReserved <118825812+mbackReserved@users.noreply.github.com>
2023-01-22 11:00:58 +0300

    Les3 (#3)
    * create models
    
    * edit models and makemigrations
    
    * create viewsets
    
    * create default api
    
    * edit models views

---
+++ userapi/todoapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from userapp.models import User
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=128)
+    url = models.URLField()
+    users = models.ManyToManyField(User)
+
+
+class TODO(models.Model):
+    text = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
---
+++ userapi/todoapp/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Project, TODO
+from userapp.serializers import UserModelSerializer, UserNameModelSerializer
+
+class ProjectModelSerializer(ModelSerializer):
+    users = UserNameModelSerializer(many=True)
+    class Meta:
+        model = Project
+        fields = ['name', 'url', 'users']
+
+class ProjectUrlModelSerializer(ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['url']
+
+
+class ToDoModelSerializer(ModelSerializer):
+    project = ProjectUrlModelSerializer()
+    author = UserNameModelSerializer()
+    class Meta:
+        model = TODO
+        fields = ['project', 'text', 'is_active', 'created_at', 'updated_at', 'author']
+
