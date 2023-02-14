eb3c42b39cb4f9d1de816935e8a3d7f0442642a6
mbackReserved <lowmustgoon@gmail.com>
2023-02-14 08:20:13 +0300

    ccreate documentation with drf-yasg

--- userapi/userapi/settings.py
+++ userapi/userapi/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'userapp',
     'todoapp',
     'django_filters',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- userapi/userapi/urls.py
+++ userapi/userapi/urls.py
@@ -14,11 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter, views
 from userapp.views import UserModelView, SingleUserView
 from todoapp.views import ProjectModelViewSet, ToDoModelViewSet
 from rest_framework.authtoken import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title = 'Userapi DRF Project',
+        default_version='1.1',
+        description='Documentation of DRF API Project for GeekBrains course',
+        contact=openapi.Contact(email='mback.edu@gmail.com'),
+        license=openapi.License(name='MIT License')
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 
 router = DefaultRouter()
@@ -32,5 +47,8 @@ urlpatterns = [
     path('users/1.2', include('userapp.urls', namespace='1.2')),
     #path('users/<int:pk>', SingleUserView.as_view()),
     path('userapi/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token)
+    path('api-token-auth/', views.obtain_auth_token),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
