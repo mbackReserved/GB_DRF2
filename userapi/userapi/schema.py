fd1f7dce43f719b82ff20512d5e6492fcb66206c
mbackReserved <lowmustgoon@gmail.com>
2023-02-18 10:11:19 +0300

    create query for todo and project_by_name

--- userapi/userapi/schema.py
+++ userapi/userapi/schema.py
@@ -15,15 +15,34 @@ class UserType(DjangoObjectType):
         fields = '__all__'
 
 
+class ToDoType(DjangoObjectType):
+    class Meta:
+        model = TODO
+        fields = '__all__'
+
+
+
 class Query(graphene.ObjectType):
     all_projects = graphene.List(ProjectType)
     all_users = graphene.List(UserType)
+    all_todo = graphene.List(ToDoType)
+    project_by_name = graphene.Field(ProjectType, name=graphene.String(required=True))
+
 
     def resolve_all_projects(self, info):
         return Project.objects.all()
 
     def resolve_all_users(self, info):
         return User.objects.all()
+
+    def resolve_all_todo(self, info):
+        return TODO.objects.all()
+    
+    def resolve_project_by_name(self, info, name):
+        try:
+            return Project.objects.get(name=name)
+        except Project.DoesNotExist:
+            return None
 
 
 schema = graphene.Schema(query=Query)
